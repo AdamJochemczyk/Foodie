6c10927e1303afb2c4c0a5592cefafcd8cbfef18
Adam Jochemczyk <adamjoche@gmail.com>
2022-01-30 18:38:13 +0100

    feat: login and register tests with supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ utils/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error("Provide supabase url");
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error("Provide supabase anon key");
+}
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
