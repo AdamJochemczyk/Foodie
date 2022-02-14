0af6ac213baad4999dcbf49acfb359e7b5aaa922
Adam Jochemczyk <adamjoche@gmail.com>
2022-02-14 12:12:35 +0100

    fix: landing page images sizes, add 404

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { OrangeButton } from "../src/common/OrangeButton/OrangeButton";
+import styles from "../styles/common.module.css";
+
+export default function Custom404() {
+  return (
+    <div className={styles.wrapper}>
+      <Image src="/static/images/404.svg" alt="404" width={800} height={800} />
+      <Link href="/" passHref>
+        <a>
+          <OrangeButton text="back to page" />
+        </a>
+      </Link>
+    </div>
+  );
+}
--- src/modules/Home/constants.ts
+++ src/modules/Home/constants.ts
@@ -25,7 +25,7 @@ export const features = [
     sectionId: "przepisy",
     title: "Brakuje Ci pomysłów na posiłek?",
     desc: "Nie ważne, czy skończyły Ci się pomysły, czy też zwyczajnie nie miałeś styczności z gotowaniem, nasz serwis umożliwi Ci przeglądanie przepisów dodanych przez użytkowników. Możesz dostosować szukanie do produktów znajdujących się w lodówce bądź do diety.",
-    imgSrc: "/static/images/landing-1.png",
+    imgSrc: "/static/images/landing-1.svg",
     blueBackground: true,
     imageRight: false
   },
@@ -33,7 +33,7 @@ export const features = [
     sectionId: "dieta",
     title: "Stwórz własną dietę",
     desc: "Dopasuj przepisy do swojej diety i stwórz własny plan posiłków. Produkty potrzebne do przygotowania wybranych potraw zostaną dodane do Twojej listy zakupów.",
-    imgSrc: "/static/images/landing-2.png",
+    imgSrc: "/static/images/landing-2.svg",
     blueBackground: false,
     imageRight: true
   },
@@ -41,7 +41,7 @@ export const features = [
     sectionId: "lodowka",
     title: "Przestań marnować żywność",
     desc: "Wiedząc, co znajduje się w Twojej lodówce i co będziesz gotować, wiesz, czego w niej brakuje. Kupujesz tylko te produkty, których potrzebujesz",
-    imgSrc: "/static/images/landing-3.png",
+    imgSrc: "/static/images/landing-3.svg",
     blueBackground: true,
     imageRight: false
   },
@@ -49,7 +49,7 @@ export const features = [
     sectionId: "zakupy",
     title: "Zakupy",
     desc: "Planowanie posiłków pozwala na kupowanie tylko tych produktów, które są potrzebne. Dzięki temu oszczędzasz pieniądze i marnujesz mniej jedzenia. Nasza lista jest tworzona automatycznie.",
-    imgSrc: "/static/images/landing-4.png",
+    imgSrc: "/static/images/landing-4.svg",
     blueBackground: false,
     imageRight: true
   },
@@ -57,7 +57,7 @@ export const features = [
     sectionId: "raporty",
     title: "Raporty",
     desc: "Dzięki naszemu narzędziu możesz śledzić postępy swojej diety. Sprawdź historię swoich posiłków.",
-    imgSrc: "/static/images/landing-5.png",
+    imgSrc: "/static/images/landing-5.svg",
     blueBackground: true,
     imageRight: false
   }
